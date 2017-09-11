b7c076d10b9fa92c97b2c931b0b0a410e645d9fd
Ian <ian@netevolved.com>
2017-09-11 13:32:36 -0400

    Initial files

---
+++ webapp/WEB-INF/base/sitemanager/catalog/events/scripts/backup/checkstatus.groovy
@@ -0,0 +1,67 @@
+package backup
+
+import java.util.logging.Logger;
+
+import org.entermediadb.asset.MediaArchive
+import org.entermediadb.email.PostMail
+import org.entermediadb.email.TemplateWebEmail
+import org.entermediadb.modules.update.Downloader
+import org.openedit.Data
+import org.openedit.OpenEditException
+import org.openedit.data.Searcher
+import org.openedit.hittracker.HitTracker
+import org.openedit.util.DateStorageUtil
+
+public void init(){
+
+	log.info("starting scan");
+	MediaArchive archive = context.getPageValue("mediaarchive");
+	HitTracker sitestomonitor = archive.getList("monitoredsites");
+	Searcher sites = archive.getSearcher("monitoredsites");
+	sitestomonitor.each{
+		Data real= archive.getData("monitoredsites", it.id);
+		String dates = 		DateStorageUtil.getStorageUtil().formatForStorage(new Date());
+		String baseurl = "${real.url}/entermedia/services/rest/systemstatus.xml";
+		try{
+			Downloader dl = new Downloader();
+			String download = dl.downloadToString(baseurl);
+			def sp = new XmlSlurper().parseText(download);
+
+			String status = sp.@stat;
+			if(status != "ok"){
+				throw new OpenEditException("Error!");
+			}
+			if(real.monitoringstatus == "error"){
+				if(real.notifyemail){
+					PostMail pm = archive.getModuleManager().getBean("postMail");
+					TemplateWebEmail email = pm.getTemplateWebEmail();
+					email.setRecipientsFromCommas(real.get("notifyemail"));
+					email.setSubject("${real.name} error resolved");
+					email.setMessage("${real.name} is running normally at ${dates}");
+					email.setFrom("monitoring@netevolved.com");
+					email.send();
+				}
+			}
+			real.setProperty("mailsent", "false");
+			real.setProperty("monitoringstatus", "ok");
+		} catch (Exception e){
+			real.setProperty("monitoringstatus", "error");
+			if(!Boolean.parseBoolean(real.get("mailsent"))){
+				if(real.notifyemail){
+					PostMail pm = archive.getModuleManager().getBean("postMail");
+					TemplateWebEmail email = pm.getTemplateWebEmail();
+					email.setRecipientsFromCommas(real.get("notifyemail"));
+					email.setSubject("${real.name} error detected");
+					email.setMessage("${real.name} has entered an error state - current time is ${dates}");
+					email.setFrom("monitoring@netevolved.com");
+					email.send();
+					real.setProperty("mailsent", "true");
+				}
+			}
+		}
+		real.setProperty("lastchecked", dates);
+		sites.saveData(real, null);
+	}
+}
+init();
+
