04093342fa88ea9b29d620b2dfcbb74895abf0ab
Cristobal <cristobal@entermediasoftware.com>
2019-08-13 13:40:42 -0500

    Delete Trial Clients

--- webapp/WEB-INF/base/entermediadb/catalog/events/scripts/utils/delete_trial_clients.groovy
+++ webapp/WEB-INF/base/entermediadb/catalog/events/scripts/utils/delete_trial_clients.groovy
@@ -25,17 +25,15 @@ public void init()
         Calendar limit = Calendar.getInstance();
         limit.add(Calendar.DAY_OF_YEAR, -15);
 
-
-        Collection expiredInstances = instanceSearcher.query().exact("instance_status","disabled").and().before("dateend", limit.getTime()).search();
+        Collection expiredInstances = instanceSearcher.query().exact("istrial", "true").exact("instance_status","disabled").and().before("dateend", limit.getTime()).search();
         log.info("Found "+expiredInstances.size()+" sites to delete.");
 
         for (Iterator instanceIterator = expiredInstances.iterator(); instanceIterator.hasNext();)
 		{
                 //Get The Client
                 Data instance = instanceSearcher.loadData(instanceIterator.next());
-				log.info("Disabling: "+instance.name+" -> "+instance.dateend);
 
-                //Get Server Info
+				//Get Server Info
                 Searcher servers = mediaArchive .getSearcher("entermedia_servers");
                 Data server = servers.query().exact("id", instance.entermedia_servers).searchOne();
                 if (server) {
@@ -53,6 +51,17 @@ public void init()
                         instance.setProperty("instance_status","deleted");
                         //client.setProperty("server","");
                         instanceSearcher.saveData(instance, null);
+						
+						//Delete Monitoring
+						Collection todelete = new ArrayList();
+						Searcher monitorsearcher = mediaArchive.getSearcher("entermedia_instances_monitor");
+						Data instancemonitor = monitorsearcher.query().match("instanceid", instance.id).searchOne();
+						if (instancemonitor) {
+							//instancemonitor.setValue("monitoringenable","false");
+							//monitorsearcher.saveData(instancemonitor, null);
+							todelete.add(instancemonitor);
+						}
+						monitorsearcher.deleteAll(todelete, null);
                 }
         }
 }
