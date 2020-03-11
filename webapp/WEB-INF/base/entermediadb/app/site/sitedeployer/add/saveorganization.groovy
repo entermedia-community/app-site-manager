c8b79af4f545e657d931d139d02a84d8c3200912
Cristobal Mejia <cristobal@openedit.org>
2020-03-11 17:18:17 -0600

    Add Organization, fixing adding followers

--- webapp/WEB-INF/base/entermediadb/app/site/sitedeployer/add/saveorganization.groovy
+++ webapp/WEB-INF/base/entermediadb/app/site/sitedeployer/add/saveorganization.groovy
@@ -9,78 +9,92 @@ import org.openedit.data.Searcher
 
 public void init()
 {
-	MediaArchive mediaArchive = context.getPageValue("mediaarchive");//Search for all files looking for videos
+	MediaArchive mediaArchive = context.getPageValue("mediaarchive");
 	BaseSearcher collectionsearcher = mediaArchive.getSearcher("librarycollection");
 	String  collectionid = data.getId();
-	LibraryCollection collection = (LibraryCollection)collectionsearcher.searchById(collectionid);
-	Searcher librarysearcher = mediaArchive.getSearcher("library");
-	log.info("User is: " + user.getId() );
-
 	
+	LibraryCollection collection = (LibraryCollection)collectionsearcher.searchById(collectionid);
+	
+	//log.info("User is: " + user.getId() );
+
+	//Create Orgainzations Library if does not exists
+	BaseSearcher librarysearcher = mediaArchive.getSearcher("library");
 	Data library = librarysearcher.searchById("organizations");
 	if( library == null)
 	{
 		library = librarysearcher.createNewData();
 		library.setId("organizations");
 		library.setValue("owner", "admin");
+		library.setValue("organizationstatus", "active");
 		library.setName("Organizations");
 		librarysearcher.saveData(library);
 	}
+	
 	collection.setValue("library", library.getId());
 	if( collection.get("owner") == null )
 	{
 		collection.setValue("owner", user.getId());
 	}	
 	collectionsearcher.saveData(collection);
-	
 	context.putPageValue("librarycol", collection);
-	log.info("Collection saved: "+collection.getId());
+	log.info("Project created: ("+collectionid+") " + collection + " Owner: "+user.getId());
+	
+	//Create General Topic if not exists
+	Searcher topicssearcher = mediaArchive.getSearcher("collectiveproject");
+	Data topicexists = topicssearcher.query().exact("parentcollectionid",collectionid).searchOne();
+	String generaltopicid=null;
+	if (topicexists == null) {
+		Data topic = topicssearcher.createNewData();
+		topic.setValue("name", "General");
+		topic.setValue("parentcollectionid", collectionid);
+		topicssearcher.saveData(topic);
+		generaltopicid = topic.getId();
+	}
+	else {
+		generaltopicid = topicexists.getId();
+	}
+	
+	//--
 	
 	//Add user as Follower and Team
 	Searcher librarycolusersearcher = mediaArchive.getSearcher("librarycollectionusers");
+	Data userexists = librarycolusersearcher.query().exact("followeruser", user.getId()).exact("collectionid", collectionid).searchOne();
 	Data librarycolusers = null;
-	librarycolusers = librarycolusersearcher.createNewData();
-	librarycolusers.setValue("collectionid", collectionid);
-	librarycolusers.setValue("followeruser", user.getId());
-	librarycolusers.setValue("ontheteam","true");
-	librarycolusersearcher.saveData(librarycolusers);
-	//--
+	if (userexists == null) {
+		librarycolusers = librarycolusersearcher.createNewData();
+		librarycolusers.setValue("collectionid", collectionid);
+		librarycolusers.setValue("followeruser", user.getId());
+		librarycolusers.setValue("ontheteam","true");
+		librarycolusersearcher.saveData(librarycolusers);
+	}
 	
-	//First Create General Topic
-	Searcher topics = mediaArchive.getSearcher("collectiveproject");
-	Data topic = topics.createNewData();
-	topic.setValue("name", "General");
-	topic.setValue("parentcollectionid", collectionid);
-	topics.saveData(topic);
-	//--
-	
-	//Notify first project only
-	Data collectionexists = librarycolusersearcher.query().exact("followeruser", user.getId()).searchOne();
-	if (collectionexists != null) {
-		String notifyuser = "168";  //Jay
-		
-		//Add Agent to Team
+	//Add Project Manager to Team
+	String projectmanager = "168";  //Jay
+	Data agentexists = librarycolusersearcher.query().exact("followeruser", projectmanager).exact("collectionid", collectionid).searchOne();
+	if (agentexists == null) {
 		librarycolusers = null;
 		librarycolusers = librarycolusersearcher.createNewData();
 		librarycolusers.setValue("collectionid", collectionid);
-		librarycolusers.setValue("followeruser", notifyuser);
+		librarycolusers.setValue("followeruser", projectmanager);
 		librarycolusers.setValue("ontheteam","true");
 		librarycolusersearcher.saveData(librarycolusers);
-		//--
-		
-		//Send Welcome Chat
-		Searcher chats = mediaArchive.getSearcher("chatterbox");
-		Data chat = chats.createNewData();
-		chat.setValue("date", new Date());
-		chat.setValue("message", "Welcome to EnterMedia! My name is Jay and I'm here as your personal support agent. If you have any questions, I'm your man. Is there anything I can help you with?");
-		chat.setValue("user", notifyuser);
-		chat.setValue("channel", topic.getId());
-		chats.saveData(chat);
-		//--
-
-		ChatManager chatmanager = (ChatManager) mediaArchive.getModuleManager().getBean(mediaArchive.getCatalogId(), "chatManager");
-		chatmanager.updateChatTopicLastModified(topic.getId());
 	}
+	//--
+		
+	//Send Welcome Chat
+	Searcher chats = mediaArchive.getSearcher("chatterbox");
+	Data chat = chats.createNewData();
+	chat.setValue("date", new Date());
+	String welcomemessage = "Welcome to the EnterMedia community. My name is Jay and I'm here as your personal support agent. Our platform is BIG, with a ton of options and features, so please don't hesitate to ask any questions you may come across. We're looking forward to working with you.";
+	chat.setValue("message", welcomemessage);
+	chat.setValue("user", projectmanager);
+	chat.setValue("channel", generaltopicid);
+	chats.saveData(chat);
+	//--
+
+	ChatManager chatmanager = (ChatManager) mediaArchive.getModuleManager().getBean(mediaArchive.getCatalogId(), "chatManager");
+	chatmanager.updateChatTopicLastModified(generaltopicid);
+	
 	mediaArchive.getProjectManager().getRootCategory(mediaArchive, collection);
 }
 
