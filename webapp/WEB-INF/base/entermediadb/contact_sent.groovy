0e4dbb357e8caa1e0f34eb14dffa8aa6544c0913
Cristobal Mejia <cristobal@openedit.org>
2020-08-05 19:12:30 -0500

    Google Captcha

---
+++ webapp/WEB-INF/base/entermediadb/contact_sent.groovy
@@ -0,0 +1,61 @@
+import org.entermediadb.email.PostMail
+import org.entermediadb.email.TemplateWebEmail
+import org.entermediadb.email.GoogleCaptcha
+import org.openedit.*
+import org.openedit.data.Searcher
+import org.openedit.data.BaseSearcher
+import org.openedit.users.*
+import org.openedit.hittracker.*
+import org.openedit.util.DateStorageUtil
+import org.openedit.util.RequestUtils
+
+		
+public void init()
+{
+		
+	String catalogid = "entermediadb/catalog";
+	String notifyemail = "cristobal@entermediadb.org";
+
+	//Send Email Notify No Seats
+	context.putPageValue("from", "noreply@entermediadb.org");
+	context.putPageValue("subject", "Contact Form");
+	context.putPageValue("form_name", context.getRequestParameter("name") );
+	context.putPageValue("form_email", context.getRequestParameter("email") );
+	context.putPageValue("form_message", context.getRequestParameter("message") );
+	
+	sendEmail(context.getPageMap(), notifyemail, "/entermediadb/contact_template.html");
+}
+
+
+//TODO: Make that table use the site (librarycollection)
+protected void sendEmail(Map pageValues, String email, String templatePage){
+	//Verify Captcha
+	String usercaptcha = context.getRequestParameter("g-recaptcha-response");
+	GoogleCaptcha captcha = (GoogleCaptcha)moduleManager.getBean("googleCaptcha");
+	captcha.setSecretKey("6LfnfroZAAAAAMHa9ixE5nIn6ScHArXl1Zys4jtz");
+	if(captcha.isValid(usercaptcha)) {
+		//send e-mail
+		RequestUtils rutil = moduleManager.getBean("requestUtils");
+		BaseWebPageRequest newcontext = rutil.createVirtualPageRequest(templatePage, null, null);
+		newcontext.putPageValues(pageValues);
+	
+		PostMail mail = (PostMail)moduleManager.getBean( "postMail");
+		TemplateWebEmail mailer = mail.getTemplateWebEmail();
+		mailer.loadSettings(newcontext);
+		mailer.setMailTemplatePath(templatePage);
+		mailer.setRecipientsFromCommas(email);
+		mailer.send();
+		context.putPageValue("error", null);
+	}
+	else {
+		log.info("Invalid Captcha");
+		context.putPageValue("error", "invalid captcha");
+	}
+	
+
+}
+
+init();
+
+
+
