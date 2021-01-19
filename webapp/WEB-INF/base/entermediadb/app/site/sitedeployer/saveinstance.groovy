a4b99b4d0e9e016ba30f868e546dd463b93c4054
Cristobal Mejia <cristobal@openedit.org>
2021-01-19 14:22:47 -0600

    Trial Sites Ansible

--- webapp/WEB-INF/base/entermediadb/app/site/sitedeployer/saveinstance.groovy
+++ webapp/WEB-INF/base/entermediadb/app/site/sitedeployer/saveinstance.groovy
@@ -15,8 +15,207 @@ import org.entermediadb.location.Position
 import org.entermediadb.projects.*
 import org.entermediadb.websocket.chat.ChatManager
 import org.openedit.Data
+import org.openedit.page.Page
+import org.openedit.util.PathUtilities
 
-public void init() 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+
+
+public void init() {
+	
+	String catalogid = "entermediadb/catalog";
+	String notifyemail = "help@entermediadb.org";
+	String clientemail = user.getEmail();
+
+	
+	String clientform = context.getSessionValue("clientform");
+	if (clientform != null) {
+		context.putSessionValue("clientform", null);
+	}
+	else {
+		context.putPageValue("errorcode", "1");
+		return;
+	}
+	String organizationid = context.getRequestParameter("collectionid");  //collectionid
+	String instanceurl = context.getRequestParameter("organization_url");
+	String instancename = context.getRequestParameter("instancename");
+	String organization_type = context.getRequestParameter("organization_type");
+	String timezone = context.getRequestParameter("timezone");
+	String region = context.getRequestParameter("region");
+	
+	if (organizationid && region) {
+		
+		//Create Valid URL
+		String selected_url = instanceurl.toLowerCase();
+		context.putPageValue("selected_url", selected_url);
+				
+		Searcher instancesearcher = searcherManager.getSearcher(catalogid, "entermedia_instances");
+		Data newinstance = instancesearcher.createNewData();
+		newinstance.setValue("librarycollection", organizationid);
+		newinstance.setValue("owner", user.getId());
+		newinstance.setValue("instance_status", "pending");
+		newinstance.setValue("name", instancename); //Needs validation?
+		context.putPageValue("instancename", instancename);
+		newinstance.setValue("instanceprefix", selected_url);
+		newinstance.setValue("istrial", true);
+		
+		instancesearcher.saveData(newinstance);
+		
+		HitTracker servers = mediaarchive.query("entermedia_servers").exact("allownewinstances", true).match("server_region", region).search();
+		Searcher serversSearcher = searcherManager.getSearcher(catalogid, "entermedia_servers");
+
+		Data server = null;
+		Integer nodeid = 0;
+		String subnetwork = "";
+		Integer maxinstances = 0;
+		Integer currentinstances = 0;
+		Boolean foundspace = false;
+		//Check if server's seat has room
+		if (servers)
+			{
+			for (Iterator serverIterator = servers.iterator(); serverIterator.hasNext();)
+			{
+				server = serversSearcher.loadData(serverIterator.next());
+				maxinstances = server.getValue("maxinstance");
+				currentinstances = server.getValue("currentinstances");
+				log.info("- Server: "+server.getName()+" M/C:"+maxinstances+"/"+currentinstances);
+				if (currentinstances < maxinstances) {
+					//--subnetwork = server.getValue("dockersubnet");
+					nodeid = server.getValue("lastnodeid") + 1;
+					foundspace = true;
+					break;
+				}
+			 }
+		
+			if (!foundspace) {
+				log.info("- No space on servers for trialsites");
+				context.putPageValue("errorcode","2");
+				
+				//Send Email Notify No Space on Servers
+				context.putPageValue("from", clientemail);
+				context.putPageValue("subject", "No space for Trial Sites");
+				sendEmail(context.getPageMap(), notifyemail,"/entermediadb/app/site/sitedeployer/email/noseats.html");
+			}
+			else{
+				// Call deploy script
+				try {
+					
+
+					JSONObject jsonObject = new JSONObject();
+					JSONArray jsonInstance = new JSONArray();
+					
+					JSONObject jsonInstanceObject = new JSONObject();
+					
+					jsonInstanceObject.put("name", instancename);
+					jsonInstanceObject.put("to", "trial0"+String.valueOf(nodeid));
+					jsonInstance.add(jsonInstanceObject);
+					
+					jsonObject.put("assigned", jsonInstance);
+	/*				
+					try {
+						String rootfolder = "/WEB-INF/temp"
+					   Page output = mediaarchive.getPageManager().getPage(rootfolder + "/deploy.json");
+					   String realpath = output.getContentItem().getAbsolutePath();
+					   File outputfile = new File(realpath);
+					   File parent = outputfile.parentFile;
+	   				   parent.mkdirs();
+					   FileWriter out = new FileWriter(outputfile);
+					   out.write(jsonObject.toJSONString());
+					   out.flush();
+					   out.close();
+						   
+					}
+					catch (IOException e) {
+									e.printStackTrace();
+					}
+					*/
+					ArrayList<String> command = new ArrayList<String>();
+					
+					command.add("-i");
+					command.add("inventory.yml trial.yml");
+					command.add("-e");
+					command.add(jsonObject.toJSONString()); //
+					
+					
+					Exec exec = moduleManager.getBean("exec");
+					ExecResult done = exec.runExec("trialsansible", command, true); //Todo: Need to move this script here?
+					log.info("- Deploying Trial Site " + selected_url + " at " + server.sshname);
+					
+						
+						String fullURL = "https://" + selected_url + "." + server.serverurl;
+						
+						newinstance.setValue("instanceurl", fullURL);
+						newinstance.setValue("instance_status", "active");
+						newinstance.setValue("instancename", selected_url);
+						newinstance.setValue("instancenode", String.valueOf(nodeid));
+						newinstance.setValue("istrial", true);
+						newinstance.setValue("entermedia_servers", server.id);
+						DateStorageUtil dateStorageUtil = DateStorageUtil.getStorageUtil();
+						newinstance.setValue("datestart", new Date());
+						newinstance.setValue("dateend", dateStorageUtil.addDaysToDate(new Date(), 30));
+						instancesearcher.saveData(newinstance);
+						
+						//Update Server
+						server.setValue("currentinstances", currentinstances + 1);
+						if (nodeid>250) {
+							server.setValue("lastnodeid", 1);
+							//server.setValue("dockersubnet", subnetwork + 1);
+						}
+						else {
+							server.setValue("lastnodeid", nodeid);
+						}
+						serversSearcher.saveData(server);
+
+						
+						context.putPageValue("userurl",fullURL);
+						
+						BaseSearcher collectionsearcher = mediaarchive.getSearcher("librarycollection");
+						Data collection = collectionsearcher.searchByField("id", organizationid);
+						if (collection) {
+							context.putPageValue("organization", collection.getValue("name"));
+						}
+						
+						context.putPageValue("newuser", "admin");
+						context.putPageValue("newpassword", "admin");
+						
+						//Add Site to Monitoring
+						//Data monitor = addNewMonitor(newinstance);
+						
+						//Send Notification to us
+						context.putPageValue("from", clientemail);
+						context.putPageValue("subject", "New Activation - " + fullURL);
+						sendEmail(context.getPageMap(), notifyemail,"/entermediadb/app/site/sitedeployer/email/salesnotify.html");
+						
+						
+						//Send Email to Client
+						context.putPageValue("from", notifyemail);
+						context.putPageValue("subject", "Welcome to EnterMediaDB ");
+						sendEmail(context.getPageMap(),clientemail,"/entermediadb/app/site/sitedeployer/email/businesswelcome.html");
+				
+					}
+					catch(Exception e){
+						 e.printStackTrace();
+					}
+			
+			}
+			}
+			else {
+				log.info("- No Servers Available.");
+				context.putPageValue("errorcode","3");
+				
+			}
+		
+	}
+	
+	
+	
+}
+
+
+
+public void init_old() 
 {
 		
 	String catalogid = "entermediadb/catalog";
